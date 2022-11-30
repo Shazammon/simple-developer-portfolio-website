862a0529a29c4bedbda0fb4b6c670d1efea6ff33
Shazammon <ammonorgill@gmail.com>
2022-11-30 14:59:24 -0700

    updated ways to contact me

--- components/AboutMe.js
+++ components/AboutMe.js
@@ -34,14 +34,14 @@ export default function AboutMe() {
                 Contact
               </h1>
               <p className="text-lg text-gray-500 mt-4 dark:text-gray-300">
-                For any sort help / enquiry, shoot a{" "}
+                For any inquiries, job opportunities, or coffee chat requests, shoot an{" "}
                 <a
                   href={`mailto:${userData.email}`}
                   className="text-gray-800 border-b-2 border-gray-800 dark:border-gray-300 font-bold dark:text-gray-300"
                 >
-                  mail
+                  email.
                 </a>{" "}
-                and I'll get back. I swear.
+                I'll be prompt to respond!
               </p>
             </div>
             <div className="mt-8">
