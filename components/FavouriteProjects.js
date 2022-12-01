a0e6f55e9da8253c1777aac3a991921c06aee0a1
Shazammon <ammonorgill@gmail.com>
2022-11-30 17:02:32 -0700

    updated footer

--- components/FavouriteProjects.js
+++ components/FavouriteProjects.js
@@ -7,7 +7,7 @@ export default function FavouriteProjects() {
       <div className="max-w-6xl mx-auto">
         <header className="flex flex-col md:flex-row justify-between items-center pt-40 mx-10 md:my-20 lg:my-0">
           <h1 className="text-6xl lg:text-9xl max-w-lg font-bold text-gray-500 my-20 md:my-0 md:text-white dark:text-gray-600 text-center">
-            Favourite Projects
+            Favorite Projects
           </h1>
           <Link
             href="/projects"
@@ -36,21 +36,21 @@ export default function FavouriteProjects() {
         <div className="grid md:grid-cols-3 gap-8 lg:-mt-8 pb-40">
           {/* Single card */}
           <a
-            href="https://tailwindmasterkit.com"
+            href="https://musiclyteachers.netlify.app/"
             className="w-full block col-span-3 shadow-2xl"
           >
             <div className="relative overflow-hidden">
               <img
-                src="/tmk.jpg"
+                src="/musicly.png"
                 alt="portfolio"
                 className="transform hover:scale-125 transition duration-2000 ease-out"
               />
-              <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
-                Tailwind Master Kit
+              <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-pink-500 rounded-md px-2">
+                Musicly
               </h1>
-              <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
+              {/* <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
                 01
-              </h1>
+              </h1> */}
             </div>
           </a>
           {/* Single card */}
@@ -61,36 +61,36 @@ export default function FavouriteProjects() {
             <div className="relative overflow-hidden">
               {/* <div className="overlay absolute inset-0 bg-black bg-opacity-70 z-10"></div> */}
               <img
-                src="/placeholdertech.png"
+                src="/"
                 alt="portfolio"
                 className="transform hover:scale-125 transition duration-2000 ease-out"
               />
-              <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
-                PlaceholderTech
+              <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-pink-500 rounded-md px-2">
+                National Park Love
               </h1>
-              <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
+              {/* <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
                 02
-              </h1>
+              </h1> */}
             </div>
           </a>
           {/* Single card */}
           <a
-            href="https://manuarora.in"
+            href="https://shazammon.github.io/p1-wheel-of-fortune/"
             className="w-full block col-span-3 sm:col-span-1  object-cover"
           >
             <div className="relative overflow-hidden shadow-2xl">
               {/* <div className="overlay absolute inset-0 bg-black bg-opacity-70 z-10"></div> */}
               <img
-                src="/portfolio.png"
+                src="/wheeloffotune.png"
                 alt="portfolio"
                 className="transform hover:scale-125 transition duration-2000 ease-out object-cover shadow-2xl"
               />
-              <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-red-500 rounded-md px-2">
-                Portfolio
+              <h1 className="absolute top-10 left-10 text-gray-50 font-bold text-xl bg-pink-500 rounded-md px-2">
+                Wheel of Fortune
               </h1>
-              <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
+              {/* <h1 className="absolute bottom-10 left-10 text-gray-50 font-bold text-xl">
                 03
-              </h1>
+              </h1> */}
             </div>
           </a>
         </div>
